5984a2b82f87c32dc44daf2ce9d44b0873a3a842
HacyViv29 <ahaciel29@gmail.com>
2024-09-18 15:48:19 -0600

    Ejercicio 1 terminado

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -7,6 +7,18 @@
 </head>
 <body>
     
+    <?php
+        include 'src/funciones.php';
+    ?>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        if(isset($_GET['numero']))
+        {
+            $num = $_GET['numero'];
+        }
+        ejer1($num);
+    ?>
 
 </body>
 </html>
---
+++ practicas/p06/src/funciones.php
@@ -0,0 +1,15 @@
+<?php
+    //Ejercicio 1
+    //Escribir programa para comprobar si un número es un múltiplo de 5 y 7.
+    function ejer1($num){
+        
+        if ($num%5==0 && $num%7==0)
+        {
+            echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+        }
+        else
+        {
+            echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+        }
+    }
+?>
