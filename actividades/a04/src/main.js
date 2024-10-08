e5c4f87e2d2d789993a9a21446e8291f2b7afab0
HacyViv29 <ahaciel29@gmail.com>
2024-10-08 09:04:16 -0600

    Ejercicio 8 realizado

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -104,3 +104,24 @@ function ejemplo7(){
         }
         }
 }
+
+function ejemplo8(){
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '');
+    
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+        case 1: document.write('uno');
+            break;
+        case 2: document.write('dos');
+            break;
+        case 3: document.write('tres');
+            break;
+        case 4: document.write('cuatro');
+            break;
+        case 5: document.write('cinco');
+            break;
+        default:document.write('debe ingresar un valor comprendido entre 1 y 5.');
+    }
+}
