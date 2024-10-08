00f64c75096c115ba101165f0a151fb71313cee9
HacyViv29 <ahaciel29@gmail.com>
2024-10-08 11:56:01 -0600

    Ejercicio 17 realizado

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -227,3 +227,29 @@ function ejemplo16(){
     valor2 = parseInt(valor2);
     mostrarRango(valor1,valor2);
 }
+
+function ejemplo17(){
+    function convertirCastellano(x) {
+        if(x==1)
+            return "uno";
+        else
+            if(x==2)
+                return "dos";
+            else
+                if(x==3)
+                    return "tres";
+                else
+                    if(x==4)
+                        return "cuatro";
+                    else
+                        if(x==5)
+                            return "cinco";
+                        else
+                            return "valor incorrecto";
+    }
+    
+    var valor = prompt("Ingresa un valor entre 1 y 5", "");
+    valor = parseInt(valor);
+    var r = convertirCastellano(valor);
+    document.write(r);
+}
