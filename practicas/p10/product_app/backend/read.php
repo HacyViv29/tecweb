8a24bb024269063448a3029820096b0591a30ac8
HacyViv29 <ahaciel29@gmail.com>
2024-10-18 15:48:29 -0600

    Modificación de prueba para mejora de busqueda

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -4,10 +4,10 @@
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
     // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['search']) ) {
-        $dato = $_POST['search'];
+    if( isset($_POST['dato']) ) {
+        $dato = $_POST['dato'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$dato}'") ) {
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$dato}' OR nombre like '%{$dato}%' OR marca like '%{$dato}%' OR detalles like '%{$dato}%'") ) {
             // SE OBTIENEN LOS RESULTADOS
 			$row = $result->fetch_array(MYSQLI_ASSOC);
 
@@ -19,25 +19,11 @@
             }
 			$result->free();
 		} else {
-            if($result = $conexion->query("SELECT * FROM productos WHERE nombre like '%{$dato}%' OR marca like '%{$dato}%' OR detalles like '%{$dato}%'") ) {
-                // SE OBTIENEN LOS RESULTADOS
-                $row = $result->fetch_array(MYSQLI_ASSOC);
-    
-                if(!is_null($row)) {
-                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                    foreach($row as $key => $value) {
-                        $data[$key] = utf8_encode($value);
-                    }
-                }
-                $result->free();
-            }
-            else{
                 die('Query Error: '.mysqli_error($conexion));
             }
         }
 		$conexion->close();
             
-    }
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
