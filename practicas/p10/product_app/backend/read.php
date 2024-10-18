24f3e30aeb5256aefa71e6b8e59ddcd491219bd1
HacyViv29 <ahaciel29@gmail.com>
2024-10-18 15:27:21 -0600

    Mejora de busqueda por más parametros

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -4,10 +4,10 @@
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
     // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
-        $id = $_POST['id'];
+    if( isset($_POST['search']) ) {
+        $dato = $_POST['search'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$dato}'") ) {
             // SE OBTIENEN LOS RESULTADOS
 			$row = $result->fetch_array(MYSQLI_ASSOC);
 
@@ -19,46 +19,24 @@
             }
 			$result->free();
 		} else {
-            die('Query Error: '.mysqli_error($conexion));
-        }
-		$conexion->close();
-    }
-    else{
-        if (isset($_POST['nombre'])) {
-            $nombre = $_POST['nombre'];
-        } else {
-            $nombre = ''; // Valor predeterminado si no se recibe 'nombre'
-        }
-
-        // Verificar si existe el valor de 'marca' en el $_POST y asignar un valor
-        if (isset($_POST['marca'])) {
-            $marca = $_POST['marca'];
-        } else {
-            $marca = ''; // Valor predeterminado si no se recibe 'marca'
-        }
-
-        // Verificar si existe el valor de 'detalles' en el $_POST y asignar un valor
-        if (isset($_POST['detalles'])) {
-            $detalles = $_POST['detalles'];
-        } else {
-            $detalles = ''; // Valor predeterminado si no se recibe 'detalles'
-        }
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        if ( $result = $conexion->query("SELECT * FROM productos WHERE nombre like '{$nombre}' OR marca like '{$marca}' OR detalles like '{$detalles}'") ) {
-            // SE OBTIENEN LOS RESULTADOS
-			$row = $result->fetch_array(MYSQLI_ASSOC);
-
-            if(!is_null($row)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
-                foreach($row as $key => $value) {
-                    $data[$key] = utf8_encode($value);
+            if($result = $conexion->query("SELECT * FROM productos WHERE nombre like '%{$dato}%' OR marca like '%{$dato}%' OR detalles like '%{$dato}%'") ) {
+                // SE OBTIENEN LOS RESULTADOS
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+    
+                if(!is_null($row)) {
+                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                    foreach($row as $key => $value) {
+                        $data[$key] = utf8_encode($value);
+                    }
                 }
+                $result->free();
             }
-			$result->free();
-		} else {
-            die('Query Error: '.mysqli_error($conexion));
+            else{
+                die('Query Error: '.mysqli_error($conexion));
+            }
         }
 		$conexion->close();
+            
     }
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
