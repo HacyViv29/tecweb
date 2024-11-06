4b2246f542579fddecaeaebd96f5916363bc39ce
HacyViv29 <ahaciel29@gmail.com>
2024-11-06 17:00:09 -0600

    Actividad 7

---
+++ actividades/a07/backend/myapi/DataBase.php
@@ -0,0 +1,21 @@
+<?php
+    namespace backend\myapi;
+
+    abstract class DataBase {
+        protected $conexion;
+
+        public function __construct( $db, $user, $pass) {
+            $this->conexion = @mysqli_connect(
+                'localhost',
+                $user,
+                $pass,
+                $db
+            );
+
+            if(!$this->conexion) {
+                die('¡Base de datos NO conextada!');
+            }
+        }
+    }
+
+?>
